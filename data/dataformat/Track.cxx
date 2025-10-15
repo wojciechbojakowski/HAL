7696b27af22d77799f1b129621d7d07363d22517
wojciechbojakowski <wojto867@gmail.com>
2025-10-15 16:37:46 +0200

    getXiinfo->nullptr test

--- data/dataformat/Track.cxx
+++ data/dataformat/Track.cxx
@@ -51,7 +51,7 @@ namespace Hal {
       v0->CopyData(other->GetV0Info());
     }
     if (IsXi()) {
-      std::cout<<"CopyData Xi part called in Track\n";
+      //std::cout<<"CopyData Xi part called in Track\n";
       fHiddenInfo = GetEvent()->fTotalXis++;
       XiTrack* xi = (XiTrack*) GetEvent()->fXisHiddenInfo->ConstructedAt(fHiddenInfo);
       xi->CopyData(other->GetXiInfo());
@@ -342,7 +342,8 @@ namespace Hal {
 
   XiTrack* Track::GetXiInfo() const {
     std::cout<<"GetXiInfo called "<<!TESTBIT(fType, kXi)<<"\n";
-    if (!TESTBIT(fType, kXi)) return nullptr;
+    if (!TESTBIT(fType, kXi)) {return nullptr;
+    std::cout<<"GetXiInfo returning nullptr\n";}
     return (XiTrack*) fEvent->fXisHiddenInfo->UncheckedAt(fHiddenInfo);
   }
 
