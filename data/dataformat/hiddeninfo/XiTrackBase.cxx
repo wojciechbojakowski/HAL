49422a5e0825712b03bbaaa88a486c9d5b2dd8fa
wojciechbojakowski <wojto867@gmail.com>
2025-11-17 00:05:38 +0100

    ehh namespace

--- data/dataformat/hiddeninfo/XiTrackBase.cxx
+++ data/dataformat/hiddeninfo/XiTrackBase.cxx
@@ -7,10 +7,12 @@
 #include "ExpEvent.h"
 #include "Track.h"
 
+namespace Hal{
 void XiTrackBase::CopyData(XiTrackBase* v){
     fTrackId     = v->fTrackId;
     fAssumedPdg  = v->fAssumedPdg;
     fDecay       = v->fDecay;
     fDecayLength = v->fDecayLength;
     fCosAngle    = v->fCosAngle;
+}
 }
