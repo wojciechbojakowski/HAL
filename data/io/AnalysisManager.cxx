32d256d78dd47b517e20320ca88dec1e8abbdf2e
wojciechbojakowski <wojto867@gmail.com>
2025-12-02 22:25:56 +0100

    hot_fix_again

--- data/io/AnalysisManager.cxx
+++ data/io/AnalysisManager.cxx
@@ -161,9 +161,11 @@ namespace Hal {
     metadata_new->AddObject(new ParameterUInt("Processed_events", fProcessedEvents, '+'));
     metadata_new->AddObject(new ParameterString("Input file", DataManager::Instance()->GetSourceName(), 'f'));
     if (!fFixRoot) {
-      if (gInterpreter->GetCurrentMacroName())
+      if (gInterpreter->GetCurrentMacroName()){
+        
         //metadata_new->AddObject(new ParameterString("Input macro", gInterpreter->GetCurrentMacroName(), 'f'));
-      else
+      }
+              else
         metadata_new->AddObject(new ParameterString("Input macro", "Loaded by user", 'f'));
     } else {
       metadata_new->AddObject(new ParameterString("Input macro", "Disabled by user", 'f'));
