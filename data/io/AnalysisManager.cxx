f1eda0c575e175db157459292b9e0d4194f26967
wojciechbojakowski <wojto867@gmail.com>
2025-12-07 13:05:21 +0100

    testy3

--- data/io/AnalysisManager.cxx
+++ data/io/AnalysisManager.cxx
@@ -163,7 +163,7 @@ namespace Hal {
     if (!fFixRoot) {
       if (gInterpreter->GetCurrentMacroName()){
         std::cout<<"Current macro name: "<<gInterpreter->GetCurrentMacroName()<<std::endl;
-        metadata_new->AddObject(new ParameterString("Input macro", 140, "f");
+        metadata_new->AddObject(new ParameterString("Input macro", Hal::Std::GetDate(), "f");
         //metadata_new->AddObject(new ParameterString("Input macro", gInterpreter->GetCurrentMacroName(), 'f'));
       }
               else
