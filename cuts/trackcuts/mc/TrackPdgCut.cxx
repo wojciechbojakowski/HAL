935df3c9673b8128b15539cccd2cfc4ea4ccaafc
wojciechbojakowski <wojto867@gmail.com>
2025-12-07 17:59:11 +0100

    cleaning

--- cuts/trackcuts/mc/TrackPdgCut.cxx
+++ cuts/trackcuts/mc/TrackPdgCut.cxx
@@ -23,7 +23,6 @@ namespace Hal {
   Bool_t TrackPdgCut::Pass(Track* track) {
     SetValue(((McTrack*) track)->GetPdg());
     double temp = ((McTrack*) track)->GetPdg();
-    std::cout << "PDG: " << temp <<", addr = "<<track<<std::endl;
     return Validate();
   }
 
